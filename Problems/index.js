0f1b59c191718519d579b6aef900bf5c0fd52a80
Charlie <chuckb1789@gmail.com>
2016-12-21 13:46:42 -0700

    Problems exercise.

---
+++ Problems/index.js
@@ -0,0 +1,22 @@
+// Q1
+console.log ('Question1');
+
+function letterCapitalize (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+console.log (letterCapitalize('word'));
+
+// Q2
+console.log ('Question2');
+
+function wordCount (string2) {
+  return string2.split(' ').length;
+}
+
+console.log (wordCount ('never eat shredded wheat'));
+
+// Q3
+console.log ('Question3')
+
+function primeTime ()
